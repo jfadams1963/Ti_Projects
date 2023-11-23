1d5b55ec1d86102d0dc963e2643b37ea2eb1b76a
J Adams <139705883+jfadams1963@users.noreply.github.com>
2023-11-23 13:25:14 -0500

    Update cryputils.c

--- tiAES-C_version/portable/src/cryputils.c
+++ tiAES-C_version/portable/src/cryputils.c
@@ -7,41 +7,41 @@
 
 //copy new state to state
 void cpyns_st() {
-	int r,c;
-	for (r=0; r<4; r++) {
-		for (c=0; c<4; c++) {
-			st[r][c] = ns[r][c];
-		}
-	}
+    int r,c;
+    for (r=0; r<4; r++) {
+        for (c=0; c<4; c++) {
+            st[r][c] = ns[r][c];
+        }
+    }
 }
 
 //copy temp block to iv
 void cpytb_iv() {
-	int r,c;
-	for (r=0; r<4; r++) {
-		for (c=0; c<4; c++) {
-			iv[r][c] = tb[r][c];
-		}
-	}
+    int r,c;
+    for (r=0; r<4; r++) {
+        for (c=0; c<4; c++) {
+            iv[r][c] = tb[r][c];
+        }
+    }
 }
 
 //copy state to temp block
 void cpyst_tb() {
-	int r,c;
-	for (r=0; r<4; r++) {
-		for (c=0; c<4; c++) {
-			tb[r][c] = st[r][c];
-		}
-	}
+    int r,c;
+    for (r=0; r<4; r++) {
+        for (c=0; c<4; c++) {
+            tb[r][c] = st[r][c];
+        }
+    }
 }
 
 //copy state to IV
 void cpyst_iv() {
-	int r,c;
-	for (r=0; r<4; r++) {
-		for (c=0; c<4; c++) {
-			iv[r][c] = st[r][c];
-		}
-	}
+    int r,c;
+    for (r=0; r<4; r++) {
+        for (c=0; c<4; c++) {
+            iv[r][c] = st[r][c];
+        }
+    }
 }
 
